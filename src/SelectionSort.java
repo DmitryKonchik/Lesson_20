3c9befa12e7f8515514eed6b171bd08b8e86c682
Dmitry <dkonchik@gmail.com>
2024-05-29 22:51:12 +0300

    Task_2 1.1

---
+++ src/SelectionSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class SelectionSort implements Runnable{
+
+    int[] sortArr;
+
+    public SelectionSort(int[] sortArr) {
+        this.sortArr = sortArr;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < sortArr.length; i++) {
+            int pos = i;
+            int min = sortArr[i];
+            for (int j = i + 1; j < sortArr.length; j++) {
+                if (sortArr[j] < min) {
+                    pos = j;
+                    min = sortArr[j];
+                }
+            }
+            sortArr[pos] = sortArr[i];
+            sortArr[i] = min;
+        }
+        System.out.println(Arrays.toString(sortArr));
+    }
+}
